f720ec141e354502a3ca1167b109004aac036243
Matt Pitner <mattpitner09@gmail.com>
2019-08-01 20:00:09 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,30 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  index = 0
+  array.each do |i|
+    if i === value_to_find
+      return index
+    end
+    index+=1
+  end
+  return nil
 end
 
 def find_max_value(array)
-  # Add your solution here
+  currentMax = array[0]
+  array.each do |i|
+    if i > currentMax
+      currentMax = i
+    end
+  end
+  return currentMax 
 end
 
 def find_min_value(array)
-  # Add your solution here
+  currentMax = array[0]
+  array.each do |i|
+    if i < currentMax
+      currentMax = i 
+    end
+  end
+  return currentMax
 end
